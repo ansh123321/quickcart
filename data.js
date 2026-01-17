2c97629f610231d13e6c3c4a10dadadd37db329c
ansh123321 <153972805+ansh123321@users.noreply.github.com>
2026-01-18 00:33:11 +0530

    Change image paths in product data
    Updated image paths in product data to use 'images' directory.

--- data.js
+++ data.js
@@ -1,9 +1,10 @@
 // Sample product data (you can add more later)
 const products = [
-  { id: 1, name: "Amul Taaza Milk 1L", price: 62, category: "Dairy", image: "image/milk.png" },
-  { id: 2, name: "Brown Bread 400g", price: 40, category: "Bakery", image: "image/bread.png" },
-  { id: 3, name: "Tomato 1kg", price: 30, category: "Vegetables", image: "image/tomato.png" },
-  { id: 4, name: "Onion 1kg", price: 28, category: "Vegetables", image: "image/onion.png" },
-  { id: 5, name: "Maggie Masala (Pack of 4)", price: 55, category: "Snacks", image: "image/maggi.png" },
-  { id: 6, name: "Lay's Chips 52g", price: 20, category: "Snacks", image: "image/chips.png" },
+  { id: 1, name: "Amul Taaza Milk 1L", price: 62, category: "Dairy", image: "images/milk.png" },
+  { id: 2, name: "Brown Bread 400g", price: 40, category: "Bakery", image: "images/bread.png" },
+  { id: 3, name: "Tomato 1kg", price: 30, category: "Vegetables", image: "images/tomato.png" },
+  { id: 4, name: "Onion 1kg", price: 28, category: "Vegetables", image: "images/onion.png" },
+  { id: 5, name: "Maggie Masala (Pack of 4)", price: 55, category: "Snacks", image: "images/maggi.png" },
+  { id: 6, name: "Lay's Chips 52g", price: 20, category: "Snacks", image: "images/chips.png" },
+
 ];
